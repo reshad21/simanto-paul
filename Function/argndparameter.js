eff2193435c1e6d5f8bc8999fc4f28c6158209f8
reshad21 <miakala7@gmail.com>
2022-08-04 10:06:28 +0600

    function nice example

---
+++ Function/argndparameter.js
@@ -0,0 +1,24 @@
+// doro tumi dokane giya bolla amake singara Dan. tahole obosoi tomake age tk dite hobe na hobe nahole dokander singara dibe na.
+
+function singara(money) {
+
+    var price = 10;
+    var quentity = money / price;
+    console.log(quentity);
+}
+// singara(50);
+
+// ai example ta cinama dakanor jonno use kora jay. dorajak cinama hall a cinamadekte hole age tk dite hoy. then hall ar modhay duke cinama dakhe ber hoye ase odience. akane kono kicu return hisabe sa pay na
+
+
+// othat tumi tk poisa diya daraiya aco kintu dokander onno kaje busy hoiya gace. kintu tumi jano tumi koyta sigara tar kac thake return paba //
+
+
+function getsingara(money) {
+    var price = 3;
+    var quentity = money / price;
+    return quentity;
+}
+
+var plate = getsingara(30);
+console.log(plate);
---
+++ Function/normalfunc.js
@@ -0,0 +1,20 @@
+// main concept of the function
+
+// dorajak tumi tomer choto vai ak sateh bed ar upore bose tv dektaco. akn tumi tomer vai ke bolla fan ta Caro.
+// just ato tukui bolla.
+
+// tahole tomer choto vai actual vabe ja kaj gula korbe.
+// step 1: sa bed theke age floor a pa felbe.
+// step 2: switch jakane ace sa khane se jabe.
+// step 3. then sa fan ar switch a click korbe.
+
+function fancaro(){
+    console.log("bed theke namo");
+    console.log("switch jakane ace sai kane jao");
+    console.log("fan ar switch a click koro");
+}
+fancaro();
+fancaro();
+console.log('ami akn parbo na amer ato bar fan carte valo lage na');
+console.log('aber dui jone a tv dekte bose porlo');
+fancaro();
